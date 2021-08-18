d80fd96e71e4e2a8b4bd11dc109b5db25bee6d4a
silverhes <rafael.hes@live.com>
2021-08-18 05:13:55 -0300

    continuação dos Estudos

---
+++ início/dados.js
@@ -0,0 +1,12 @@
+console.log("para iniciar variáveis basta colocar o nome da variável depois do comando let ou var")
+
+let nome = "Caneta"
+let quantidade = 10
+let preco = 6.4
+let imposto = 1.5
+
+console.log(nome)
+console.log(quantidade)
+console.log(preco)
+console.log(imposto)
+console.log(preco + imposto)

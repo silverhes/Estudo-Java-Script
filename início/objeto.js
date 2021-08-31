6584d15285dc2d054766049afa46eeda268d4f2f
silverhes <rafael.hes@live.com>
2021-08-31 10:40:52 -0300

    Create objeto.js

---
+++ início/objeto.js
@@ -0,0 +1,16 @@
+//Objeto!!
+
+const prod1 = {} //criando um tipo Objeto
+prod1.nome = 'Galaxy S20' //criando atributos para o objeto e já dar valor para ele.
+prod1.preco = 4999.90
+prod1['Desconto legal'] = 0.4 //identificadores com espaço MAS TEM QUE SER EVITADO POR BOAS PRÁTICAS
+
+console.log(prod1)
+
+const prod2 {//outra forma de criar atributos
+    nome: 'Camisa Polo'
+    preco: 79.90
+}
+
+console.log((prod2))
+
